8956b6c1d2e4fd9abdfb6fe5d6da4d02ea1c757d
Julia Kalinke <juliasennholz@web.de>
2024-10-10 20:18:43 +0200

    - Neue Schicht "Sonstiges" - Neue Schicht "Fütterer" - MVA-Schichten als Akkordion

--- src/components/shifts/ShiftSelectionModal.js
+++ src/components/shifts/ShiftSelectionModal.js
@@ -1,13 +1,34 @@
 
 import ReactModal from 'react-modal';
 import ReactScrollableList from 'react-scrollable-list';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Accordion, List } from '@mui/material';
+
 
 export default function ShiftSelectionModal({ isOpen, onChooseSelection, onCancel }) {
 
     const SHIFTS = require('../../constants/Shifts.json');
 
+    const mvaListItems = Object.keys(SHIFTS).filter((key) => SHIFTS[key].shiftID.startsWith("mva")).map((key) => {
+        const style = {
+            backgroundColor: SHIFTS[key].bg,
+            color: SHIFTS[key].textColor
+        };
+        const button = <button
+            onClick={() => onChooseSelection(SHIFTS[key])}
+            className='SelectionButton'
+            style={style}
+        >{SHIFTS[key].name}</button>
 
-    const listItems = Object.keys(SHIFTS).map((key) => {
+        console.log(key);
+        return {
+            id: SHIFTS[key].shiftID, content: button,
+        }
+    });
+
+    const listItems = Object.keys(SHIFTS).filter((key) => !SHIFTS[key].shiftID.startsWith("mva")).map((key) => {
         const style = {
             backgroundColor: SHIFTS[key].bg,
             color: SHIFTS[key].textColor
@@ -23,6 +44,15 @@ export default function ShiftSelectionModal({ isOpen, onChooseSelection, onCance
         }
     });
 
+
+    const accordionStyle = {
+        margin: "4px 0px",
+        bordererRadius: "5px!important",
+        backgroundColor: "#0a98ff",
+        color: "white",
+
+    }
+
     return (
             <ReactModal 
           className={"Modal"}
@@ -30,6 +60,25 @@ export default function ShiftSelectionModal({ isOpen, onChooseSelection, onCance
            isOpen={isOpen}
         >
             <div className='ModalContainer'>
+                <Accordion
+                    sx={accordionStyle}>
+                    <AccordionSummary
+                        sx={{justifyContent: "center"}}
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          MVA
+        </AccordionSummary>
+                    <AccordionDetails>
+                        <ReactScrollableList
+                    className='ScrollList'
+                    listItems = {mvaListItems}
+                    heightOfItem={30}
+                    style={{ color: '#333' }}
+                />
+        </AccordionDetails>
+      </Accordion>
                 <ReactScrollableList
                     className='ScrollList'
                     listItems = {listItems}
